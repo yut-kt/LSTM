12287d6efeeb38e680a56a3f8c67773c9850a8a5
yut-kt <yut.kt59@gmail.com>
2018-11-23 09:02:23 +0900

    fix: iterator

--- evaluation.py
+++ evaluation.py
@@ -6,9 +6,10 @@ from argparse import ArgumentParser
 
 def main():
     with open(args.answer_file) as a, open(args.input_file) as i:
+        a = a.readlines()
+        i = i.readlines()
         pre_list = [x == y for x, y in zip(a, i) if y.strip() == '1']
         rec_list = [x == y for x, y in zip(a, i) if x.strip() == '1']
-        print(rec_list)
 
         precision = len(list(filter(bool, pre_list))) / len(pre_list) if pre_list else 0
         recall = len(list(filter(bool, rec_list))) / len(rec_list) if rec_list else 0

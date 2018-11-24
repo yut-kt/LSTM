6268bc44b66e0f7853d0d1ad48c8a11bb22e1234
yut-kt <yut.kt59@gmail.com>
2018-11-24 12:30:32 +0900

    delete: precision

--- gridSearch_LSTM_byKeras.py
+++ gridSearch_LSTM_byKeras.py
@@ -35,34 +35,6 @@ def main():
     for mean, stdev, param in zip(means, stds, params):
         print("%f (%f) with: %r" % (mean, stdev, param))
 
-    npz = np.load(args.npz_test_file)
-    test_sentences = npz['sentences']
-    tests_length = len(test_sentences)
-    test_labels = npz['labels']
-    test_sequences = tokenizer.texts_to_sequences(test_sentences)
-    test = pad_sequences(test_sequences, max_length, padding='pre').reshape(tests_length, 1, max_length)
-    test_labels = [label.argmax() for label in test_labels]
-
-    results = [result.argmax() for result in grid.predict(test)]
-
-    pre_list = [x == int(y) for x, y in zip(test_labels, results) if y == 1 or y == '1']
-    rec_list = [x == int(y) for x, y in zip(test_labels, results) if x == 1 or x == '1']
-
-    precision = len(list(filter(bool, pre_list))) / len(pre_list) if pre_list else 0
-    precision_round = Decimal(str(precision * 100)).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
-
-    recall = len(list(filter(bool, rec_list))) / len(rec_list) if rec_list else 0
-    recall_round = Decimal(str(recall * 100)).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
-
-    f_measure = 2 * precision * recall / (precision + recall) if precision + recall != 0 else 0
-    f_measure_round = Decimal(str(f_measure * 100)).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
-
-    print('---------- Evaluation ----------')
-    print(f'{"precision":<9}：{f"{len(list(filter(bool, pre_list)))} / {len(pre_list)}":<9} = {precision_round}%')
-    print(f'{"recall":<9}：{f"{len(list(filter(bool, rec_list)))} / {len(rec_list)}":<9} = {recall_round}%')
-    print(f'{"F-measure":<9}：{"2pr/(p+r)":<9} = {f_measure_round}%')
-    print('--------------------------------')
-
 
 def create_model(recurrent_activation1='hard_sigmoid',
                  recurrent_activation2='hard_sigmoid',
